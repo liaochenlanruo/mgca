04c1453a771505859a29dc44885a83a1a800f8e5
liaochenlanruo <liaochenlanruo@webmail.hzau.edu.cn>
2022-04-26 15:53:16 +0800

    Update build.sh

--- conda/build.sh
+++ conda/build.sh
@@ -2,6 +2,7 @@
 mkdir -p $PREFIX/bin
 
 cp mgca.pl $PREFIX/bin/mgca
+cp Scripts/setupDB.pl $PREFIX/bin/
 cp Scripts/pI/print_pI.pl $PREFIX/bin/
 cp Scripts/pI/plot_pI.R $PREFIX/bin/
 cp Scripts/IS/run_islandpath.pl $PREFIX/bin/
@@ -12,6 +13,7 @@ cp Scripts/CRISPR/db/cas1_db.* $PREFIX/bin/
 
 
 chmod a+x $PREFIX/bin/mgca
+chmod a+x $PREFIX/bin/setupDB.pl
 chmod a+x $PREFIX/bin/print_pI.pl
 chmod a+x $PREFIX/bin/plot_pI.R
 chmod a+x $PREFIX/bin/run_islandpath.pl
